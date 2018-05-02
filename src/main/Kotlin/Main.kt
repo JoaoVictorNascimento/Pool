ed2eeb929475b210cd647e29a23b76270d3738b0
Thiago <thiagohoh@users.noreply.github.com>
2018-05-02 18:04:23 -0300

    Update

--- src/main/Kotlin/Main.kt
+++ src/main/Kotlin/Main.kt
@@ -1,12 +1,18 @@
 import db.DbConn
-
+import model.Person
 
 
 fun main(args: Array<String>) {
 
     val epa = DbConn() //Serie de testes da conexao do banco
+    //epa.supercon()
+   // epa.selectNameFromP()
+
     epa.supercon()
-    epa.selectNameFromP()
+   var lisss :MutableList<Person> = epa.selectPerson()
+    val test :Person = lisss[0]
+
+    println(test.printero())
 
 
 
