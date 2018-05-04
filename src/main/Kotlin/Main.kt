f96c193ec6e3f9136c6d61aeda049b2df0e9baf6
Thiago <thiagohoh@users.noreply.github.com>
2018-05-03 21:46:31 -0300

    Update main
    Contains commented tests

--- src/main/Kotlin/Main.kt
+++ src/main/Kotlin/Main.kt
@@ -1,10 +1,10 @@
 import db.DbConn
 import model.Person
+import model.Student
 
 
 fun main(args: Array<String>) {
 
-    
     val epa = DbConn() //Serie de testes da conexao do banco
     //epa.supercon()
    // epa.selectNameFromP()
@@ -12,13 +12,22 @@ fun main(args: Array<String>) {
    epa.supercon()
    var lisss :MutableList<Person> = epa.selectPerson()
    val test :Person = lisss[0]
-   println(test.printero())
+   //println(test.printero())
 
-    val ptes = Person(100,"testerson",9742,321)
+    //val ptes = Person(100,"testerson",9742,321) create person
+    //val auTest = model.Class(1,"3:40","Segunda") create class
 
-    epa.supercon()
-    epa.insertPessoa(ptes)
+   // epa.supercon()
+    //epa.insertClasss(auTest) insert class
+
+    //epa.insertPessoa(ptes) insert person
+    //val testStudent :Student = Student(1,test.getIdPessoa(),auTest.getIdAula()) create studant
+
+    //epa.supercon()
+    //epa.insertStudent(testStudent) insert student
 
 
 
 }
+
+
