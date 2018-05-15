cba1f7f07aac3522abb46e40c2d1bb5a72c0c1b1
Franklin <franklinjunior12@hotmail.com>
2018-05-14 21:40:38 -0300

    Update Person.kt

--- src/main/Kotlin/model/Person.kt
+++ src/main/Kotlin/model/Person.kt
@@ -3,21 +3,21 @@ package model
 
 class Person {
 
-    private var idPessoa: Int
+    //private var idPessoa: Int
     private var nome: String
     private var rg: Int
     private var cpf: Int
 
-    constructor(idPessoa: Int, nome: String, rg: Int, cpf: Int) {
-        this.idPessoa = idPessoa
+    constructor(nome: String, rg: Int, cpf: Int) {
+        //this.idPessoa = idPessoa
         this.nome = nome
         this.rg = rg
         this.cpf = cpf
     }
 
-    fun getIdPessoa() :Int{
-            return idPessoa
-    }
+   // fun getIdPessoa() :Int{
+   //         return idPessoa
+    //}
 
     fun getNome() :String{
         return nome
@@ -32,7 +32,7 @@ class Person {
     }
 
     fun printero(){
-        println("Name:" +this.nome+" Id:" +this.idPessoa +" RG:" +this.rg +" CPF:"+this.cpf)
+        println("Name:" +this.nome+" RG:" +this.rg +" CPF:"+this.cpf)
     }
 
 }
