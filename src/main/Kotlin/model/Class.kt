9e360e303474fb0d070ec29a4fd0efb5e362d3fe
Thiago <thiagohoh@users.noreply.github.com>
2018-05-27 04:39:28 -0300

    dataclass

--- src/main/Kotlin/model/Class.kt
+++ src/main/Kotlin/model/Class.kt
@@ -2,33 +2,4 @@ package model
 
 
 
-class Class{
-
-
-    private var idAula: Int
-    private var horario: String
-    private var dia: String
-
-    constructor(idAula :Int, horario :String, dia :String){
-        this.idAula = idAula
-        this.horario = horario
-        this.dia = dia
-    }
-
-    fun getIdAula():Int{
-        return idAula
-    }
-
-    fun getHorario():String{
-        return horario
-    }
-
-    fun getDia():String{
-        return dia
-    }
-
-    fun printero(){
-        println("IAula: "+ this.idAula+ "Horario: "+this.horario +"Dia:"+ this.dia )
-    }
-
-}
+data class Class(val idAula :Int,val horario :String,val dia :String)
