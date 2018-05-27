5cbf601fbb3e4a579b10276b968d0f20662c13dd
Thiago <thiagohoh@users.noreply.github.com>
2018-05-27 04:38:41 -0300

    update
    data class

--- src/main/Kotlin/model/Student.kt
+++ src/main/Kotlin/model/Student.kt
@@ -2,33 +2,7 @@ package model
 
 
 
-class Student{
-
-    private var idAluno: Int
-    private var pessoaIdPessoa: Int
-    private var aulaIdAula: Int
-
-    constructor(idAluno:Int, pessoaIdPessoa:Int, aulaIdAula: Int){
-
-        this.idAluno = idAluno
-        this.pessoaIdPessoa = pessoaIdPessoa
-        this.aulaIdAula = aulaIdAula
-
-    }
-
-
-    fun getIdAluno():Int{
-        return idAluno
-    }
-
-    fun getPessoaIdPessoa():Int{
-        return pessoaIdPessoa
-    }
-
-    fun getAulaIdAula():Int{
-        return aulaIdAula
-    }
-
+data class Student(val idAluno:Int,val pessoaIdPessoa:Int,val aulaIdAula: Int){
 
     fun printero(){
         println("IAluno: "+ this.idAluno+ "PidP: "+this.pessoaIdPessoa +"AidAula:"+ this.aulaIdAula )
