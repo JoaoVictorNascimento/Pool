3c31ff398afff6583bb855e1536d7f321ae5f399
Thiago <thiagohoh@users.noreply.github.com>
2018-05-27 04:39:46 -0300

    dataclass

--- src/main/Kotlin/model/Contract.kt
+++ src/main/Kotlin/model/Contract.kt
@@ -2,36 +2,4 @@ package model
 
 
 
-class Contract{
-
-
-
-    private var idContract: Int
-    private var fileContract: String
-    private var alunoIdAluno: Int
-
-
-    constructor(idContract: Int, fileContract: String,alunoIdAluno: Int){
-
-        this.idContract = idContract
-        this.fileContract = fileContract
-        this.alunoIdAluno = alunoIdAluno
-
-    }
-
-
-    fun getIdContract():Int{
-        return idContract
-    }
-
-    fun getFileContract() :String{
-        return fileContract
-    }
-    fun getAlunoIdAluno() :Int{
-        return alunoIdAluno
-    }
-
-
-
-
-}
+data class Contract(val idContract: Int,val fileContract: String,val alunoIdAluno: Int)
