ddd44813be89049dbd1ea209d10a0c24ed65f7dc
Thiago <thiagohoh@users.noreply.github.com>
2018-06-07 22:55:07 -0300

    test dir

---
+++ test/kotlin/model/PersonDaoTest.kt
@@ -0,0 +1,40 @@
+package model
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class PersonDaoTest {
+    @Test
+    fun getCon() {
+    }
+
+    @Test
+    fun setCon() {
+    }
+
+    @Test
+    fun save() {
+    }
+
+    @Test
+    fun findById() {
+    }
+
+    @Test
+    fun findByName() {
+    }
+
+    @Test
+    fun update() {
+    }
+
+    @Test
+    fun delete() {
+    }
+
+    @Test
+    fun selectAll() {
+    }
+
+}
