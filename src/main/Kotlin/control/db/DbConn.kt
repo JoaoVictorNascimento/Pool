a4847182cc5f5a19515054c76ed782bc1e94a930
Thiago <thiago14lost@gmail.com>
2018-07-04 22:48:03 -0300

    clean

--- src/main/Kotlin/control/db/DbConn.kt
+++ src/main/Kotlin/control/db/DbConn.kt
@@ -16,39 +16,27 @@ import java.sql.*
 
 class DbConn{
 
-private var conn :Connection? = null
-private var stat :Statement? = null
-private var res :ResultSet? = null
+    private var conn :Connection? = null
+    private var stat :Statement? = null
+    private var res :ResultSet? = null
 
-fun supercon(): Connection? { //conexao com o banco
+    fun supercon(): Connection? { //conexao com o banco
 
-    val jFile = Filer()
-    val theUrl = jFile.filero()
+        val jFile = Filer()
+        val theUrl = jFile.filero()
 
-    try{
-        Class.forName("com.mysql.cj.jdbc.Driver").newInstance()
-        conn = DriverManager.getConnection(theUrl[0],theUrl[1],theUrl[2])
-    }catch (ex: SQLException){
-        ex.printStackTrace()
-    }catch (ex: Exception){
-        ex.printStackTrace()
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance()
+            conn = DriverManager.getConnection(theUrl[0],theUrl[1],theUrl[2])
+        }catch (ex: SQLException){
+            ex.printStackTrace()
+        }catch (ex: Exception){
+            ex.printStackTrace()
+        }
+        return conn
     }
-    return conn
 }
 
-
-    fun insertContract(cont :Contract){
-
-        val statement = conn!!.prepareStatement("INSERT INTO contrato" +
-                "(idContrato, Arquivo, Aluno_idAluno) VALUES (?,?,?) ")
-        statement.setInt(1, cont.idContract )
-        statement.setString(2, cont.fileContract)
-        statement.setInt(3, cont.alunoIdAluno)
-        statement.executeUpdate()
-
-    }
-
-}
 
 
 
